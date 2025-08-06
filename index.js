601a64eb8449a70b6047103273091715390f8c5f
mellissafelicianna <mellissafelicianna@gmail.com>
2025-08-06 22:18:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const CHAT_ID = "8425195586";
 async function sendTelegramMessage(message) {
   const url = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
   try {
- await axios.post(url, {
+await axios.post(url, {
   chat_id: CHAT_ID,
-  text: message,
-  parse_mode: "Markdown",
+  text: message
 });
+
 
   } catch (err) {
     console.error("Telegram verzendfout:", err.response?.data || err.message);
