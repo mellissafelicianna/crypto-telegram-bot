490f0789cdd77e5c94852eaf838db00437a7d61c
mellissafelicianna <mellissafelicianna@gmail.com>
2025-08-06 19:09:32 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,13 @@ module.exports = {
     {
       name: "crypto-telegram-bot",
       script: "index.js",
-      watch: false,
+      watch: true,
       autorestart: true,
       max_restarts: 10,
       restart_delay: 5000,
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
+        PORT: 3000
       }
     }
   ]
